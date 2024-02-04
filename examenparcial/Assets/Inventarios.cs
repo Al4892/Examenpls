ae542ac556776337c1ccf50f62cef0f2376e06ea
Al4892 <alan.iniguez@uteca.edu.mx>
2024-02-04 01:52:38 -0600

    s

--- examenparcial/Assets/Inventarios.cs
+++ examenparcial/Assets/Inventarios.cs
@@ -3,57 +3,168 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditorInternal;
 using UnityEditor;
-
+using UnityEngine.UI;
 
 public class Inventarios : MonoBehaviour
 {
-    
-    
+
+
     public List<GameObject> Bag = new List<GameObject>();
-    public GameObject inv;
+    public GameObject[] inv;
     public bool Activarinv;
     public GameObject selector;
     public int id;
     public string item_activo;
+    public List<Image> Equipo = new List<Image>();
+    public int id_equipo;
+    public int fasesinv;
+
+    public GameObject Opciones;
+    public Image[] Seleccion;
+    public Sprite[] Selection_Sprite;
+    public int Id_Selected;
+
+
     //[Header("Objeto activo")]
     //[SerializeField]
     public void navegar()
     {
-        if (Input.GetKeyDown(KeyCode.D) && id < Bag.Count - 1)
+        switch (fasesinv)
         {
-            id++;
+            case 0:
+                selector.SetActive(true);//
+                Opciones.SetActive(false);//
+
+                inv[1].SetActive(false); 
+                if(Input.GetKeyDown(KeyCode.W)&& id_equipo>0) 
+                {
+                    id_equipo--;
+                
+                
+                } 
+                if(Input.GetKeyDown(KeyCode.S)&& id_equipo< Equipo.Count-1) 
+                {
+                    id_equipo++;
+                
+                
+                }
+                selector.transform.position = Equipo[id_equipo].transform.position;
+                if (Input.GetKeyDown(KeyCode.F) && Activarinv)
+                {
+                    fasesinv = 1;
+                }
+            break;
+                case 1:
+                selector.SetActive(true);//
+                Opciones.SetActive(false);//
+                if(Input.GetKeyDown(KeyCode.F)&& Bag[id].GetComponent<Image>().enabled==true)//
+                {
+                    fasesinv = 2;
+
+                }
+                inv[1].SetActive(true);
+                if (Input.GetKeyDown(KeyCode.D) && id < Bag.Count - 1)
+                {
+                    id++;
+                }
+                if (Input.GetKeyDown(KeyCode.A) && id > 0)
+                {
+                    id--;
+                }
+                if (Input.GetKeyDown(KeyCode.W) && id > 3)
+                {
+                    id -= 4;
+                }
+                if (Input.GetKeyDown(KeyCode.S) && id < 8)
+                {
+                    id += 4;
+                }
+                selector.transform.position = Bag[id].transform.position;
+                if(Input.GetKeyDown(KeyCode.G)&& Activarinv) 
+                {
+                    fasesinv = 0;
+                
+                }
+                break;
+                case 2:
+                if (Input.GetKeyDown(KeyCode.G))
+                {
+                    fasesinv = 1;
+                }
+                Opciones.SetActive (true);
+                Opciones.transform.position= Bag[id].transform.position;
+                selector.SetActive (false);
+                if (Input.GetKeyDown(KeyCode.W)&&Id_Selected>0)
+                {
+                    Id_Selected--;
+                }
+                if (Input.GetKeyDown(KeyCode.S) && Id_Selected < Seleccion.Length - 1)
+                {
+                    Id_Selected++;
+                }
+                switch (Id_Selected)
+                {
+                    case 0:
+                        Seleccion[0].sprite = Selection_Sprite[1];
+                        Seleccion[1].sprite = Selection_Sprite[0];
+                        if (Input.GetKeyDown(KeyCode.F))
+                        {
+                            if (Equipo[id_equipo].GetComponent<Image>().enabled == false)
+                            {
+                                Equipo[id_equipo].GetComponent<Image>().sprite = Bag[id].GetComponent<Image>().sprite;
+                                Equipo[id_equipo].GetComponent<Image>().enabled = true;
+                                Bag[id].GetComponent<Image>().sprite = null;
+                                Bag[id].GetComponent<Image>().enabled = false;
+                            }
+                            else
+                            {
+                                Sprite obj = Bag[id].GetComponent<Image>().sprite;
+
+                                Bag[id].GetComponent<Image>().sprite = Equipo[id].GetComponent<Image>().sprite;
+                                Equipo[id_equipo].GetComponent < Image>().sprite = obj;
+
+                            }
+                            fasesinv = 0;
+
+                        }
+                        break;
+                        case 1:
+                        Seleccion[0].sprite = Selection_Sprite[0];
+                        Seleccion[1].sprite = Selection_Sprite[1];
+                        if (Input.GetKeyDown(KeyCode.F))
+                        {
+                            Bag[id].GetComponent<Image>().sprite = null;
+                            Bag[id].GetComponent<Image>().enabled = false; ;
+                            fasesinv = 1;
+
+                        }
+                        break;
+                }
+                break;
         }
-        if (Input.GetKeyDown(KeyCode.A) && id > 0)
-        {
-            id--;
-        }
-        if (Input.GetKeyDown(KeyCode.W) && id >3)
-        {
-            id-=4;
-        }
-        if (Input.GetKeyDown(KeyCode.S) && id <8)
-        {
-            id += 4;
-        }
-        selector.transform.position = Bag[id].transform.position;
+
+
+
+
+       
     }
     
-    private void OnTriggerEnter2D(Collider2D collision)
+     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Item"))
         {
-         /*   
+           
             for(int i = 0; i < Bag.Count; i++)
             {
-                if (Bag[i].gameObject.GetComponent<imageprite>().enabled == false)
+                if (Bag[i].GetComponent<Image>().enabled == false)
                 {
 
                  Bag[i].GetComponent<Image>().enabled = true;
-                 Bag[i].GetComponent<ima>.sprite = collision.GetComponent<SpriteRenderer>().sprite;
+                 Bag[i].GetComponent<Image>().sprite = collision.GetComponent<SpriteRenderer>().sprite;
                     break;
                 }
             }
-            */
+            
         }
         
     }
@@ -68,11 +179,12 @@ public class Inventarios : MonoBehaviour
         navegar();
         if (Activarinv)
         {
-            inv.SetActive(true);
+            inv[0].SetActive(true);
         }
         else
         {
-            inv.SetActive(false);
+            fasesinv= 0;
+            inv[0].SetActive(false);
         }
         if (Input.GetKeyDown(KeyCode.K))
         {
